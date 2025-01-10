d6a45c36db8b38ee49bc1ec1bddf824ab2541e36
CanGuv <babagvn1903@gmail.com>
2025-01-10 00:17:45 +0000

    Update email_sender.py

--- Emailsender/email_sender.py
+++ Emailsender/email_sender.py
@@ -5,15 +5,15 @@ from pathlib import Path
 
 html = Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from'] = 'Babacan Guven'
-email['to'] = 'babagvn1903@gmail.com'
-email['subject'] = 'You won £1,000,000'
+email['from'] = 'Bob' # Name
+email['to'] = 'john@gmail.com' # Email address
+email['subject'] = 'Check up'
 
 email.set_content(html.substitute(name = 'Bob'), 'html')
 
-with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp: # For gmail, but you can change it
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('babaqpr@gmail.com', 'tqwglncozlqkvmdh')
+	smtp.login('bob@gmail.com', 'hshsjjsjsjs') # Sender's email address and hashed password (which you can obtain from your email settings)
 	smtp.send_message(email)
 	print('all good')
